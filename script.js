ff9719ddb03cad082ff5cad52c75da1907185aee
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-05-29 11:12:54 +0100

    mobile completion and media Q's

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 const images = document.querySelectorAll(".image1");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 const about = document.querySelector(".about-section");
 const contact = document.querySelector(".contact-section");
 const services = document.querySelector(".service-section");
 
 let activeSlide = 0;
 
-setInterval(() => {
-  activeSlide++;
-
-  if (activeSlide > images.length - 1) {
-    activeSlide = 0;
-  }
-  setActiveImage();
-}, 8000);
-
 function setActiveImage() {
   images.forEach((img) => img.classList.remove("active"));
 
   images[activeSlide].classList.add("active");
 }
+
+function prevslide() {
+  prev.addEventListener("click", () => {
+    activeSlide--;
+
+    if ((activeSlide = 0)) {
+      return;
+    }
+
+    setActiveImage();
+  });
+}
+
+function nextslide() {
+  next.addEventListener("click", () => {
+    activeSlide++;
+
+    if (activeSlide > images.length - 1) {
+      activeSlide = 0;
+    }
+
+    setActiveImage();
+  });
+}
+
+function init() {
+  nextslide();
+  prevslide();
+}
+
+init();
 
 document.querySelector(".links").addEventListener("click", function (e) {
   e.preventDefault();
