c1270d7386eaf8dc77c5859731538c3471448c46
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-05-31 19:45:42 +0100

    nav animation

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const next = document.querySelector(".next");
 const about = document.querySelector(".about-section");
 const contact = document.querySelector(".contact-section");
 const services = document.querySelector(".service-section");
+const submit = document.querySelector(".fcf-btn");
 
 let activeSlide = 0;
 
@@ -45,11 +46,37 @@ function init() {
 init();
 
 document.querySelector(".links").addEventListener("click", function (e) {
-  e.preventDefault();
-
   // Matching strategy
   if (e.target.classList.contains("nav-link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
+});
+
+$(document).ready(function () {
+  $(".fcf-btn").click(function (e) {
+    let email = $(".email").val();
+    let name = $(".name").val();
+    let message = $(".message").val();
+    let statusEl = $(".status");
+    statusEl.empty();
+
+    if (email.length > 5 && email.includes("@") && email.includes(".")) {
+    } else {
+      e.preventDefault();
+      statusEl.append("<p>Email is not valid!</p>");
+    }
+
+    if (message.length > 15) {
+    } else {
+      e.preventDefault();
+      statusEl.append("<p>Message is not valid!</p>");
+    }
+
+    if (name.length > 2) {
+    } else {
+      e.preventDefault();
+      statusEl.append("<p>Name is not valid!</p>");
+    }
+  });
 });
